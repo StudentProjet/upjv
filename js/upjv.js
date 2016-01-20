63c086acb7864e1ba832ec602e2757a6efe2aaa9
kperin <kristopher.perin@gmail.com>
2016-01-20 16:48:16 +0100

    Init

---
+++ js/upjv.js
@@ -0,0 +1,10 @@
+var $ = jQuery;
+
+$(document).ready(function() {
+
+    /*if($(window).width < 769){
+        $("#menu_principal li a").bind("click", function(){
+            $(this).parents().closest("ul").toggle();
+        });
+    }*/
+});
