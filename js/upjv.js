98ac3885223d2573be496131e5a5f62678039513
kperin <kristopher.perin@gmail.com>
2016-05-05 11:10:41 +0200

    maj front

--- js/upjv.js
+++ js/upjv.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     });
+
+    if($('#menu_principal li a').hasClass("is-active")){
+        $('#menu_principal li a.is-active').css("color","#666666");
+    }
+    
+    if($('#menu_principal li ul li a').hasClass("is-active")){
+        $('#menu_principal li ul li a.is-active').parent().parent().css("display", "block");
+        $('#menu_principal li ul li a.is-active').parent().parent().prev().css("color","#666666");
+        $('#menu_principal li ul li a.is-active').css("color","#FFF");
+    }
+
 });
